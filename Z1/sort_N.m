886df1c975e555129ddfdfbffbc1ed3fa44466ca
Frozen <mrozix11@gmail.com>
2018-03-03 20:15:21 +0100

    Added sorting function

--- Z1/sort_N.m
+++ Z1/sort_N.m
@@ -1,7 +1,18 @@
-function [ output_args ] = sort_N( input_args )
-%SORT_N Summary of this function goes here
-%   Detailed explanation goes here
-
-
+function [ ] = sort_N( )%input_args )
+global model_database;
+[rows,columns] = size(model_database);
+selected_column = 'A';
+determinant = [];
+for i=1:1:rows
+    determinant = [determinant; det(eval(sprintf('model_database(%d).%s', i, selected_column)))];
+end
+[B,I] = sort(determinant);
+more on;
+sorted = [];
+for i=1:1:rows
+   sorted = [sorted; model_database(I(i)).name];
+end
+sorted
+more off;
 end
 
