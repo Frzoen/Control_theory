47eb1f985422e623c0701718bb7dec88b1a024f6
Frozen <mrozix11@gmail.com>
2018-03-04 20:23:24 +0100

    Added read_k function

--- Z1/read_k.m
+++ Z1/read_k.m
@@ -1,7 +1,11 @@
-function [ output_args ] = read_k( input_args )
-%READ_K Summary of this function goes here
-%   Detailed explanation goes here
-
-
+function [ ] = read_k( )
+global model_database; % load database from workspace
+tmp_model = struct ('name','','A','','B','','C','','D',''); %define temporary stuct
+tmp_model.name = input('Enter system name (i.e. "name") : ');
+tmp_model.A = eval(input('Enter matrix A (i.e. "[1,2;3,4]") : '));
+tmp_model.B = eval(input('Enter matrix B (i.e. "[1;2]") : '));
+tmp_model.C = eval(input('Enter matrix C (i.e. "[1,2]") : '));
+tmp_model.D = eval(input('Enter matrix D (i.e. "[1]") : '));
+model_database=[model_database;tmp_model]; %push temporary to database
 end
 
